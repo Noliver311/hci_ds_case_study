2859caf2bbec10b0f33dee2633a1a50f489a1202
Noliver311 <oliver@amendllc.com>
2024-06-06 14:02:17 -0400

    Upload files

---
+++ forecastmodel.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df_10_consumption = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_010_consumption.csv")
+df_10_holiday = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_010_holiday.csv")
+df_10_weather = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_010_weather.csv")
+df_30_consumption = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_030_consumption.csv")
+df_30_holiday = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_030_holiday.csv")
+df_30_weather = pd.read_csv(r"C:\Users\NicholasOliver\OneDrive - amendllc.com\DS_Case_Study\2024-04-ds_cast_study_data\division_030_weather.csv")
